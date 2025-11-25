89514de17091847743167d92b67c081b53f6991c
Shawon <shahidur8381@gmail.com>
2025-11-26 00:55:52 +0600

    IMPLEMENTED DATABASE SYSTEM

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,6 @@
 module com.example._207103_project {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
     requires java.sql;
 
     opens com.example._207103_project to javafx.fxml;
